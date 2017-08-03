db8deaf059808d0d18dda6be8a91d842ba82891d
Alan Cave <acave@versalogic.com>
2017-08-03 15:55:50 -0700

    First commit

---
+++ mx6qtetra_config/include/generated/version_autogenerated.h
@@ -0,0 +1,4 @@
+#define PLAIN_VERSION "2016.03-imx_v2016.03_4.1.15_2.0.0_ga+ga57b13b"
+#define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
+#define CC_VERSION_STRING "arm-poky-linux-gnueabi-gcc (GCC) 5.3.0"
+#define LD_VERSION_STRING "GNU ld (GNU Binutils) 2.26.0.20160214"

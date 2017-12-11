506eb83b8ec85671c7e4c433da9a577cf82c415f
Alan Cave <acave@versalogic.net>
2017-12-11 14:24:04 -0800

    change ddr refresh rate

--- mx6qtetra_config/include/generated/timestamp_autogenerated.h
+++
@@ -1,4 +0,0 @@
-#define U_BOOT_DATE "Dec 11 2017"
-#define U_BOOT_TIME "13:49:40"
-#define U_BOOT_TZ "-0800"
-#define U_BOOT_DMI_DATE "12/11/2017"
--- mx6qtetra_config/include/generated/version_autogenerated.h
+++
@@ -1,4 +0,0 @@
-#define PLAIN_VERSION "2016.03+fslc+g8382cf8"
-#define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
-#define CC_VERSION_STRING "arm-poky-linux-gnueabi-gcc (GCC) 5.3.0"
-#define LD_VERSION_STRING "GNU ld (GNU Binutils) 2.26.0.20160214"
